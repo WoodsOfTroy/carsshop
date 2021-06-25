69be9ac1989eaa7b88d6da14ef75ffc2ac161d2e
Ruslan <badalov2605@gmail.com>
2021-06-25 18:33:28 +0300

    v1.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-let cars = [{brand: 'BMW', price:70000, color:'black', model:'m5'}, 
-        {brand: 'BMW', price:18000, color:'white', model:'E39'},
-        {brand: 'Dodge', price:100000, color:'red', model:'Chalenger'},
-        {brand: 'Dodge', price:180000, color:'red', model:'Viper'},
-        {brand: 'AUDI', price:200000, color:'yellow', model:'R8'},
-        {brand: 'AUDI', price:38000, color:'black', model:'A3'},
-        {brand: 'Bugatti', price:1700000, color:'blue', model:'Veyron'},
-        {brand: 'Aston Martin', price:170000, color:'orange', model:'Rapid'},
-        {brand: 'Toyota', price:10000, color:'red', model:'Yaris'},
-        {brand: 'Toyota', price:25000, color:'black', model:'RAV4'},
-        {brand: 'Land Rover', price:30000, color:'green', model:'Defender'},
-        {brand: 'Range Rover', price:78000, color:'black', model:'Sport'},
-        {brand: 'Skoda', price:20000, color:'white', model:'Octavia'},
-        {brand: 'Skoda', price:15000, color:'blue', model:'Fabia'}];
+let cars = [{brand: 'BMW', price:70000, color:'black', model:'m5', photo: 'img/bmw_m5.jpg'}, 
+        {brand: 'BMW', price:18000, color:'white', model:'E39', photo: 'img/bmwe39.jpg'},
+        {brand: 'Dodge', price:100000, color:'red', model:'Challenger', photo: 'img/dodge_challenger.jpg'},
+        {brand: 'Dodge', price:180000, color:'red', model:'Viper', photo: 'img/dodge_viper.jpg'},
+        {brand: 'AUDI', price:200000, color:'yellow', model:'R8', photo: 'img/audi_r8.jpg'},
+        {brand: 'AUDI', price:38000, color:'black', model:'A3', photo: 'img/audi_a3.jpg'},
+        {brand: 'Bugatti', price:1700000, color:'blue', model:'Veyron', photo: 'img/bugatti_veyron.jpg'},
+        {brand: 'Aston Martin', price:170000, color:'orange', model:'Rapid', photo: 'img/am_rapid.jpg'},
+        {brand: 'Toyota', price:10000, color:'red', model:'Yaris', photo: 'img/toyota_yaris.jpg'},
+        {brand: 'Toyota', price:25000, color:'black', model:'RAV4', photo: 'img/toyota_rav4.jpg'},
+        {brand: 'Land Rover', price:30000, color:'green', model:'Defender', photo: 'img/lr_defender.jpg'},
+        {brand: 'Range Rover', price:78000, color:'black', model:'Sport', photo: 'img/rr_sport.jpg'},
+        {brand: 'Skoda', price:20000, color:'white', model:'Octavia', photo: 'img/scoda_octavia.jpg'},
+        {brand: 'Skoda', price:15000, color:'blue', model:'Fabia', photo: 'img/Skoda_Fabia.jpg'}];
 
 $.each(cars, function(index, car){   // Функция заполнения селект-списка
     $('#color').first().append(`<option value="" selected>--Выберите цвет--</option>`).append(`<option value="${car.color}">${car.color}</option>`);
@@ -22,7 +22,7 @@ $("#color option").val(function(idx, val) {
   });
 $(document).ready(function(){               // Функция вывода полного списка автомобилей
     $.each(cars, function(index, car){
-        $('.cars').append(`<div class="car"><div class="photo">photo</div><div class="specification"><h1>${car.brand} ${car.model}</h1><span>Color: ${car.color}</span><h2>${car.price}</h2></div></div>`);
+        $('.cars').append(`<div class="car"><div class="photo"><a data-fancybox="gallery" href="${car.photo}"><img src="${car.photo}" alt="${car.brand} ${car.model}"></a></div><div class="specification"><h1>${car.brand} ${car.model}</h1><span>Color: ${car.color}</span><h2>${car.price}</h2></div></div>`);
     });
 });
 
@@ -158,8 +158,13 @@ $('button').click(function(){            // Выполнения по нажат
         }
     $.each(sortedCars, function(index, car)     //Выведение отсортированного массива на экран
     {
-        $('.cars').append(`<div class="car"><div class="photo">photo</div><div class="specification"><h1>${car.brand} ${car.model}</h1><span>Color: ${car.color}</span><h2>${car.price}</h2></div></div>`);
+        $('.cars').append(`<div class="car"><div class="photo"><a data-fancybox="gallery" href="${car.photo}"><img src="${car.photo}" alt="${car.brand} ${car.model}"></a></div><div class="specification"><h1>${car.brand} ${car.model}</h1><span>Color: ${car.color}</span><h2>${car.price}</h2></div></div>`);
     });
       
     
-})
+});
+
+$("body").niceScroll({        //Новый скрол
+    cursorcolor: "#3e4a5c",
+    cursorwidth: "20px"
+});
